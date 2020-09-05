b24ecc8b6cbe8e6951f18cf8bb52ffd66ecf3380
JanPapaj-reload <68897923+JanPapaj-reload@users.noreply.github.com>
2020-09-06 00:40:46 +0200

    po uprave: 1. zredukoval jsem for smycky 2. doplnil immutable kolekci 3. bohuzel si k prevedeni str vstupnich cisel na int pomohl funkci map(). Je tu jine reseni? 4. ted se pul hodiny snazil vymyslet mechanismus, jak osetrit vlozeni nepovolenych znaku, ale je 00:40 a uz do toho jen tupe ziram a predstiram soustredene kognitivni usili :)  Dnes uz jiste ne!
    valim spat

--- for_search.py
+++ for_search.py
@@ -17,55 +17,52 @@ target = input("Which word should I look for?").strip(",./- ")
 for index, word in enumerate(text):
     if word.strip(".,") == target:
         print(f"The index of your word is: {index + 1}")
+    else:
+        print("The word has not been found.")
+        break
 
 # ask user for sequence of integers, use 'q' to quit from asking user for next number
 total = 0
+reply_list = []
 while True:
     reply = input("Insert a number (or 'q' to quit):  ").lower().strip()
     if reply != "q":
         total += int(reply)
-        continue
+        reply_list.append(reply)
     else:
         print("Total:", total)
         break
 
 # get maximum, minimum and average of the integers in a list without a build in functions
-list = [1,3,5,8,9,45,21,0,3]
-max = 1
-min = 1
-total = 0
-for num in list:
+max = 0
+min = 0
+for num in list(map(int, reply_list)):
     if num > max:
         max = num
-        continue
-print("Max:", max)
-
-for num in list:
-    if num < min:
+    elif num < min:
         min = num
-print("Min:", min)
 
-for num in list:
-    total += num
-average = round(total / len(list), 2)
+print("Max:", max)
+print("Min:", min)
+average = round(total / len(reply_list), 2)
 print("Average:", average)
 
 # create immutable copy a list and clear original data
-
+immutable = tuple(map(int, reply_list))
+print(immutable)
+reply_list.clear()
 
 # BONUS: can you also print the indexes of maximum and minimum?
-list = [1,3,5,8,9,45,21,0,3]
-max = 1
-min = 1
-for i, num in enumerate(list):
+max = 0
+min = 0
+index_max = 0
+index_min = 0
+for i, num in enumerate(immutable):
     if num > max:
         max = num
-        index = i
-        continue
-print("The index of max value:", index)
-for i, num in enumerate(list):
-    if num < min:
+        index_max = i
+    elif num < min:
         min = num
-        index = i
-        continue
-print("The index of min value:", index)
+        index_min = i
+print("The index of max value:", index_max)
+print("The index of min value:", index_min)

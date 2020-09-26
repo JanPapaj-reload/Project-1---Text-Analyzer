e2c74b5c9ef7a24d183c114e5373dd7b6dd80bf5
JanPapaj-reload <68897923+JanPapaj-reload@users.noreply.github.com>
2020-09-26 11:30:01 +0200

    dlouhy vikend, takze hotovo. komentare primo ve scriptu.

--- PROJECT 1 Text analyzer.py
+++ PROJECT 1 Text analyzer.py
@@ -1,5 +1,5 @@
 '''
-author =
+author =  Jan Papaj
 '''
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer, 
@@ -38,305 +38,143 @@ dict_hesla = {
     "liz" : "pass123",
 }
 
-# 1. Welcome to app
-print("-" * 80)
-print("Hi there. Long time no see.")
+import sys
+split_text = []
+text_selection = 0
+approved = False
 
-# 2. username and password, stripped of potential whitespaces and uppercase
-username = input("Type in your username: ").lower().strip(" ")
-password = input("And now your password: ").lower().strip(" ")
+def line_separator():
+    print("-"*50)
 
-# 3. membership test for name and password
-while username not in dict_hesla.keys():
-    username = input("This username is not registered. Try inserting it again: ").lower().strip()
-while password != dict_hesla[username]:
-    password = input("The password is not valid either. Try inserting it again: ").lower().strip()
-print("Username:", username)
-print("Password:", password)
 
-print("-" * 80)
+def login():
+    login_attempt = 0
+    print("Hello. Welcome to the app.")
+    while login_attempt < 2:
+        username = input("Please, type in your username: ").lower().strip(" ")
+        password = input("And now your password: ").strip(" ")
+        if username in dict_hesla.keys() and password == dict_hesla[username]:
+            print("Welcome,", username,". Your login has been successful.")
+            break
+        login_attempt += 1
+    else:
+        print("Login credentials incorrect. Please, contact Helpdesk.")
+        sys.exit()
 
-# 4. text selection input
-print("We have 3 texts to be analyzed.")
-text_selection = int(input("Select a number 1-3 to choose a specific text: "))
-print("Alright then, let's now crunch Text", text_selection,":")
-print("-"*80)
-# 5. text operations
-# splitting the texts into iterable lists of strings, getting rid of commas and periods
-split_text1_1 = TEXTS[0].replace(",", " ")
-split_text1_2 = split_text1_1.replace(".", " ")
-split_text1_3 = split_text1_2.split()
 
-split_text2_1 = TEXTS[1].replace(",", " ")
-split_text2_2 = split_text2_1.replace(".", " ")
-split_text2_3 = split_text2_2.split()
 
-split_text3_1 = TEXTS[2].replace(",", " ")
-split_text3_2 = split_text3_1.replace(".", " ")
-split_text3_3 = split_text3_2.split()
+# def text_select():
+#     print("We have 3 texts to be analyzed.")
+#     text_selection = input("Select a number 1-3 to choose a specific text: ")
+#     try:
+#         if str(text_selection.isalpha()) == True:
+#             text_selection = input("You have to key in a number between 1 and 3: ")
+#         elif not 1 <= int(text_selection) <= 3:
+#             text_selection = input("You have to key in a number between 1 and 3: ")
+#     except:
+#         text_selection = input("Key in a NUMBER between 1 and 3: ")
+#     print("Alright then, let's now crunch Text", text_selection, ".")
 
-# variable for uppercase / lowercase membership test
-uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-             "U", "V", "W", "X", "Y", "Z"]
 
-# empty variables for word frequency bar representation
-two_words = []
-three_words = []
-four_words = []
-five_words = []
-six_words = []
-seven_words = []
-eight_words = []
-nine_words = []
-ten_words = []
-eleven_words = []
-twelve_words = []
-thirteen_words = []
-fourteen_words = []
-fourteen_plus_words = []
+# def splitt_text(text_selection):
+#     split_text = TEXTS[(text_selection - 1)]
+#     split_text = split_text.replace(",.!?", " ")
+#     split_text = split_text.split()
+#     return split_text
 
-if text_selection == 1:
-    # words total
-    total_text1_3 = len(split_text1_3)
-    print("In Text 1 there are", total_text1_3, "words,")
-    # num of titlecased
-    capital_text1_3 = []
-    for item in split_text1_3:
-        if item[0] in uppercase:
-            capital_text1_3.append(item)
-    print("of which", len(capital_text1_3),"are capitalized.")
-    # num of uppercased
-    uppercase_text1_3 = []
-    for item in split_text1_3:
-        if item.isupper() == True:
-            uppercase_text1_3.append(item)
-    print("There are also", len(uppercase_text1_3), "uppercase")
-    # num of lowercased
-    lowercase_text3_3 = []
-    for item in split_text1_3:
-        if item.islower() == True:
-            lowercase_text3_3.append(item)
-    print("and", len(lowercase_text3_3), "lowercase words.")
-    # num of all-numeric
-    numeric_text3_3 = []
-    for item in split_text1_3:
-        if item.isalnum():
-            numeric_text3_3.append(item)
-    print("Last but not least, there are", len(numeric_text3_3), "all-numeric strings.")
-    print("-"*80)
-    # graphic bar representation
-    for item in split_text1_3:
-        if len(item) == 2:
-            two_words.append(item)
-        elif len(item) == 3:
-            three_words.append(item)
-        elif len(item) == 4:
-            four_words.append(item)
-        elif len(item) == 5:
-            five_words.append(item)
-        elif len(item) == 6:
-            six_words.append(item)
-        elif len(item) == 7:
-            seven_words.append(item)
-        elif len(item) == 8:
-            eight_words.append(item)
-        elif len(item) == 9:
-            nine_words.append(item)
-        elif len(item) == 10:
-            ten_words.append(item)
-        elif len(item) == 11:
-            eleven_words.append(item)
-        elif len(item) == 12:
-            twelve_words.append(item)
-        elif len(item) == 13:
-            thirteen_words.append(item)
-        elif len(item) == 14:
-            fourteen_words.append(item)
+
+def words_sum(split_text):
+    result = len(split_text)
+    print("There are", result, "words in that particular text")
+
+
+def titlecase(split_text):
+    result = 0
+    for word in split_text:
+        if word.istitle():
+            result += 1
+    print("There are", result, "titlecase words")
+
+
+def uppercase(split_text):
+    result = 0
+    for word in split_text:
+        if word.isupper():
+            result += 1
+    print("There are", result, "uppercase words")
+
+
+def lowercase(split_text):
+    result = 0
+    for word in split_text:
+        if word.islower():
+            result += 1
+    print("There are", result, "lowercase words")
+
+
+def all_numeric(split_text):
+    result = 0
+    for numeric in split_text:
+        if numeric.isnumeric():
+            result += 1
+    print("There are", result, "numeric words")
+
+# tady jsem si nekonecne dlouho na webu pohledal mozna reseni. pokus/omyl :(
+# snad me to teda aspon neco naucilo
+def frequency(split_text):
+    len_freq = {}
+    for x in split_text:
+        if len(x) in len_freq:
+            len_freq[len(x)] += 1
         else:
-            fourteen_plus_words.append(item)
-    print("2", len(two_words) * "*", len(two_words))
-    print("3", len(three_words) * "*", len(three_words))
-    print("4", len(four_words) * "*", len(four_words))
-    print("5", len(five_words) * "*", len(five_words))
-    print("6", len(six_words) * "*", len(six_words))
-    print("7", len(seven_words) * "*", len(seven_words))
-    print("8", len(eight_words) * "*", len(eight_words))
-    print("9", len(nine_words) * "*", len(nine_words))
-    print("10", len(ten_words) * "*", len(ten_words))
-    print("11", len(eleven_words) * "*", len(eleven_words))
-    print("12", len(twelve_words) * "*", len(twelve_words))
-    print("13", len(thirteen_words) * "*", len(thirteen_words))
-    print("14", len(fourteen_words) * "*", len(fourteen_words))
-    print("14+", len(fourteen_plus_words) * "*", len(fourteen_plus_words))
-    print("-"*80)
-    # sum of all-numeric words
-    sum_1 = 0
-    for item in split_text1_3:
-        if item.isdigit() is True:
-            sum_1 += int(item)
-    print("...and the sum of all numeric words here is", sum_1)
-
-elif text_selection == 2:
-    # words total
-    total_text2_3 = len(split_text2_3)
-    print("In Text 2 there are", total_text2_3, "words,")
-    # num of titlecased
-    capital_text2_3 = []
-    for item in split_text2_3:
-        if item[0] in uppercase:
-            capital_text2_3.append(item)
-    print("of which", len(capital_text2_3), "are capitalized.")
-    # num of uppercased
-    uppercase_text2_3 = []
-    for item in split_text2_3:
-        if item.isupper() == True:
-            uppercase_text2_3.append(item)
-    print("There are also", len(uppercase_text2_3), "uppercase")
-    # num of lowercased
-    lowercase_text2_3 = []
-    for item in split_text2_3:
-        if item.islower() == True:
-            lowercase_text2_3.append(item)
-    print("and", len(lowercase_text2_3), "lowercase words.")
-    # num of all-numeric
-    numeric_text2_3 = []
-    for item in split_text2_3:
-        if item.isalnum():
-            numeric_text2_3.append(item)
-    print("Last but not least, there are", len(numeric_text2_3), "all-numeric strings.")
-    print("-" * 80)
-    # graphic bar representation
-    for item in split_text2_3:
-        if len(item) == 2:
-            two_words.append(item)
-        elif len(item) == 3:
-            three_words.append(item)
-        elif len(item) == 4:
-            four_words.append(item)
-        elif len(item) == 5:
-            five_words.append(item)
-        elif len(item) == 6:
-            six_words.append(item)
-        elif len(item) == 7:
-            seven_words.append(item)
-        elif len(item) == 8:
-            eight_words.append(item)
-        elif len(item) == 9:
-            nine_words.append(item)
-        elif len(item) == 10:
-            ten_words.append(item)
-        elif len(item) == 11:
-            eleven_words.append(item)
-        elif len(item) == 12:
-            twelve_words.append(item)
-        elif len(item) == 13:
-            thirteen_words.append(item)
-        elif len(item) == 14:
-            fourteen_words.append(item)
-        else:
-            fourteen_plus_words.append(item)
-    print("2", len(two_words) * "*", len(two_words))
-    print("3", len(three_words) * "*", len(three_words))
-    print("4", len(four_words) * "*", len(four_words))
-    print("5", len(five_words) * "*", len(five_words))
-    print("6", len(six_words) * "*", len(six_words))
-    print("7", len(seven_words) * "*", len(seven_words))
-    print("8", len(eight_words) * "*", len(eight_words))
-    print("9", len(nine_words) * "*", len(nine_words))
-    print("10", len(ten_words) * "*", len(ten_words))
-    print("11", len(eleven_words) * "*", len(eleven_words))
-    print("12", len(twelve_words) * "*", len(twelve_words))
-    print("13", len(thirteen_words) * "*", len(thirteen_words))
-    print("14", len(fourteen_words) * "*", len(fourteen_words))
-    print("14+", len(fourteen_plus_words) * "*", len(fourteen_plus_words))
-    print("-" * 80)
-    # sum of numeric words
-    sum_1 = 0
-    for item in split_text2_3:
-        if item.isdigit() is True:
-            sum_1 += int(item)
-    print("...and the sum of all numeric words here is", sum_1)
-
-else:
-    # words total
-    total_text3_3 = len(split_text3_3)
-    print("In Text 3 there are", total_text3_3, "words,")
-    # num of titlecased
-    capital_text3_3 = []
-    for item in split_text3_3:
-        if item[0] in uppercase:
-            capital_text3_3.append(item)
-    print("of which", len(capital_text3_3), "are capitalized.")
-    # num of uppercased
-    uppercase_text3_3 = []
-    for item in split_text3_3:
-        if item.isupper() == True:
-            uppercase_text3_3.append(item)
-    print("There are also", len(uppercase_text3_3), "uppercase")
-    # num of lowercased
-    lowercase_text3_3 = []
-    for item in split_text3_3:
-        if item.islower() == True:
-            lowercase_text3_3.append(item)
-    print("and", len(lowercase_text3_3), "lowercase words.")
-    # num of all-numeric
-    numeric_text3_3 = []
-    for item in split_text3_3:
-        if item.isalnum():
-            numeric_text3_3.append(item)
-    print("Last but not least, there are", len(numeric_text3_3), "all-numeric strings.")
-    print("-" * 80)
-    # graphic bar representation
-    for item in split_text3_3:
-        if len(item) == 2:
-            two_words.append(item)
-        elif len(item) == 3:
-            three_words.append(item)
-        elif len(item) == 4:
-            four_words.append(item)
-        elif len(item) == 5:
-            five_words.append(item)
-        elif len(item) == 6:
-            six_words.append(item)
-        elif len(item) == 7:
-            seven_words.append(item)
-        elif len(item) == 8:
-            eight_words.append(item)
-        elif len(item) == 9:
-            nine_words.append(item)
-        elif len(item) == 10:
-            ten_words.append(item)
-        elif len(item) == 11:
-            eleven_words.append(item)
-        elif len(item) == 12:
-            twelve_words.append(item)
-        elif len(item) == 13:
-            thirteen_words.append(item)
-        elif len(item) == 14:
-            fourteen_words.append(item)
-        else:
-            fourteen_plus_words.append(item)
-    print("2", len(two_words) * "*", len(two_words))
-    print("3", len(three_words) * "*", len(three_words))
-    print("4", len(four_words) * "*", len(four_words))
-    print("5", len(five_words) * "*", len(five_words))
-    print("6", len(six_words) * "*", len(six_words))
-    print("7", len(seven_words) * "*", len(seven_words))
-    print("8", len(eight_words) * "*", len(eight_words))
-    print("9", len(nine_words) * "*", len(nine_words))
-    print("10", len(ten_words) * "*", len(ten_words))
-    print("11", len(eleven_words) * "*", len(eleven_words))
-    print("12", len(twelve_words) * "*", len(twelve_words))
-    print("13", len(thirteen_words) * "*", len(thirteen_words))
-    print("14", len(fourteen_words) * "*", len(fourteen_words))
-    print("14+", len(fourteen_plus_words) * "*", len(fourteen_plus_words))
-    print("-" * 80)
-    # sum of numeric words
-    sum_1 = 0
-    for item in split_text3_3:
-        if item.isdigit() is True:
-            sum_1 += int(item)
-    print("...and the sum of all numeric words here is", sum_1)
-print("-"*80)
+            len_freq.setdefault(len(x), 1)
+    grouped = list(len_freq.keys())
+    grouped.sort()
+    for y in grouped:
+        print( y, "|", '*' * len_freq[y], len_freq[y])
 
 
+def sum_numeric(split_text):
+    result = 0
+    for numeric in split_text:
+        if numeric.isnumeric():
+            result += int(numeric)
+    print("If we sum up all numbers, we will get", float(result))
+
+
+def text_analyzer(texts):
+    line_separator()
+    while login():
+        return
+    line_separator()
+    # text_selection()    <- puvodne jsem chtel volat jako funkci,
+    # v ramci celeho programku text_analyzer, ale motal jsem se v tom
+    print("We have 3 texts to be analyzed")
+    text_selection = int(input("Choose a number between 1-3: "))
+    # pokousel jsem se osetrit alpha znaky na vstupu pomoci try/except, ale utopil jsem se.
+    # zatim tedy tak, ac to blbuvzdorny neni. vim o tom
+    while not 1 <= text_selection <= 3:
+        text_selection = int(input(("It must be a NUMBER between 1 and 3! ")))
+    else:
+        print("Alright, let's crunch text", text_selection)
+    line_separator()
+
+    # splitt_text(text_selection)  <-  takto jsem funkci volal a interpreter to nevzal, viz lines 63-80
+
+    split_text = texts[text_selection-1]
+    split_text = split_text.replace(",.!?", " ")
+    split_text = split_text.split()
+
+    words_sum(split_text)
+    titlecase(split_text)
+    uppercase(split_text)
+    lowercase(split_text)
+    all_numeric(split_text)
+    line_separator()
+    frequency(split_text)
+    line_separator()
+    sum_numeric(split_text)
+    line_separator()
+
+# line 176 mi neustale vyhazuje jako chybovou. proc???
+text_analyzer(TEXTS)

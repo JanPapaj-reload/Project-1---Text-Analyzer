35a5025291ccb00cf38989e3d28faca2744242a1
JanPapaj-reload <68897923+JanPapaj-reload@users.noreply.github.com>
2020-10-11 12:40:01 +0200

    hoj. odstranil jsem vetsinu chyb. grabnul userMove stup pomoci exceptions. nove nadefinoval funkci na vepisovani vybranych pozic userem a compem, ale program jako takovy se chova chaoticky - nekdy comp policko zabere, vetsinou vubec. Je to zase neco maleho, prehlednuteho, prosim?

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,5 +1,5 @@
 import random
-# creates general scope for variable move and board
+# creates general scope for these variables
 board = [' ' for pos in range(10)]
 move = 0
 
@@ -10,7 +10,11 @@ def printBoard(board):
         print('------------------')
         print(' ', board[7], ' | ', board[8], ' | ', board[9], ' ')
 
-# returns bool whether the space if free, move-1 as I need to ignore index 0
+# handles chosen moves by user and comp
+def inputLetter(letter, move):
+    board[move] = letter
+
+# returns bool whether space if free, move-1 as I need to ignore index 0
 def isBoxFree(board, move):
     return board[int(move)] == ' '
 
@@ -20,7 +24,14 @@ def boardNotFull(board):
 
 # chaining bool values for all winning combinations; not possible to cut up lines after 'or' or with '\n'
 def isWinner(board, char):
-    return (board[1] == char and board[2] == char and board[3] == char) or (board[4] == char and board[5] == char and board[6] == char) or (board[7] == char and board[8] == char and board[9] == char) or (board[1] == char and board[4] == char and board[7] == char) or (board[2] == char and board[5] == char and board[8] == char) or (board[3] == char and board[6] == char and board[9] == char) or (board[1] == char and board[5] == char and board[9] == char) or (board[7] == char and board[5] == char and board[3] == char)
+    return (board[1] == char and board[2] == char and board[3] == char) \
+           or (board[4] == char and board[5] == char and board[6] == char) \
+           or (board[7] == char and board[8] == char and board[9] == char) \
+           or (board[1] == char and board[4] == char and board[7] == char) \
+           or (board[2] == char and board[5] == char and board[8] == char) \
+           or (board[3] == char and board[6] == char and board[9] == char) \
+           or (board[1] == char and board[5] == char and board[9] == char) \
+           or (board[7] == char and board[5] == char and board[3] == char)
 
 
 def userMove(board):
@@ -31,8 +42,7 @@ def userMove(board):
         try:
             if 0 < int(move) < 10:
                 if isBoxFree(board,move):
-                    board[int(move)] = 'X'
-                    validMove = True
+                    inputLetter('X', int(move))
                 else:
                     print("The box is taken. Choose again.")
             else:
@@ -44,6 +54,7 @@ def userMove(board):
 
 
 def compMove(board):
+    move = 0
     # generates all possible moves that comp can take
     possibleMoves = []
     for x, letter in enumerate(board):
@@ -56,36 +67,48 @@ def compMove(board):
     # series of elif statements in order of priority from top to bottom; where comp needs to move next
     for x in possibleMoves:
         if isWinner(testBoard, 'O'):
-            move = x
+            # return inputLetter('O', move)
             return move
         elif 5 in possibleMoves:
-            move = 5
+            # return inputLetter('O', move)
             return move
         elif x in [1,3,7,9]:
             possibleCorners = []
             possibleCorners.append(x)
             if len(possibleCorners) > 0:
                 move = random.choice(possibleCorners)
+            # return inputLetter('O', move)
             return move
         elif x in [2,4,6,8]:
             possibleEdges = []
             if len(possibleEdges) > 0:
                 move = random.choice(possibleEdges)
+            # return inputLetter('O', move)
             return move
-    return move
+    return inputLetter('O', move)
 # one elif statement missing - how comp can block the user from winning on next move.
 
+# prepracoval jsem while cyklus pro stridani user- a compMove.
+# nadefinoval jsem nove funcki pro vepisovani zvoleneho pole. tam jsem se musel insirovat na webu.
+# kazdopadne program se chova nahodile - nekdy comp policko obsadi, casto vubec ne. nemuzu prijit na to proc.
 def program():
     print('Ready to play TicTacToe? Let\'s start.')
     printBoard(board)
     while boardNotFull(board):
-        userMove(board)
-        if isWinner(board, 'X'):
-            print("You beat the computer.")
-            break
-        compMove(board)
-        if isWinner(board, 'O'):
-            print('The Computer wins. Better luck next time.')
+        if not isWinner(board, 'O'):
+           userMove(board)
+           inputLetter('X', move)
+           if isWinner(board, 'X'):
+                print("You beat the computer.")
+                break
+
+        if not isWinner(board, 'X'):
+            compMove(board)
+            inputLetter('O', move)
+            if isWinner(board, 'O'):
+                print("The computer beat you. Better luck next time.")
+                break
+
     else:
         print("It's a tie game. Nobody wins.")
 
